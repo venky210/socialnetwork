09685547ef667ff9e935c7c14868af5f0f653331
karthikvembuluru <meckarthik250@gamil.com>
2024-05-07 12:59:45 +0530

    changes

--- todolist1/app/forms.py
+++ todolist1/app/forms.py
@@ -10,7 +10,7 @@ class userform(forms.ModelForm):
         help_texts={'username':''}
 
 class TaskForm(forms.ModelForm):
-    is_task = forms.BooleanField(label='Task is completed',required=False)
+  
     class Meta:
         model=Task
-        fields=['title', 'description','is_task']                                                     
+        fields=['title', 'description','completed']                                                     
