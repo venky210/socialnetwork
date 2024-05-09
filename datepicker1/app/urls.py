6a1801ac9415b7df2971f22039fdae9c163fca80
karthikvembuluru <meckarthik250@gamil.com>
2024-05-09 17:17:35 +0530

    changes

--- datepicker1/app/urls.py
+++ datepicker1/app/urls.py
@@ -6,5 +6,16 @@ urlpatterns = [
         path('date_display/', date_display, name='date_display'),
         path('create_time/', create_selected_time, name='create_selected_time'),
         path('time_list/', selected_time_list, name='selected_time_list'),
+        path('',homepage,name='homepage'),
+        path('registration/',registration,name='registration'),
+        path('user_login/',user_login,name='user_login'),
+        path('add_task/',add_task,name='add_task'),
+        path('view_tasklist/',view_tasklist,name='view_tasklist'),
+        path('search/', search_tasks, name='search_tasks'),
+        path('logout/', logout, name='logout'),
+        path('update_task/<int:task_id>/', update_task, name='update_task'),
+        path('delete_task/<int:task_id>/', delete_task, name='delete_task'),
+        path('pending_tasks/', pending_tasks, name='pending_tasks'),
+        path('completed_tasks/',completed_tasks, name='completed_tasks'),
 
 ]
