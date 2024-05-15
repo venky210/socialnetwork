1708ca29531da164a6fa61761041aa83cb55b1fb
karthikvembuluru <meckarthik250@gamil.com>
2024-05-15 10:42:53 +0530

    changes

--- datepicker1/app/models.py
+++ datepicker1/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=100)
     completed = models.BooleanField(default=False)
