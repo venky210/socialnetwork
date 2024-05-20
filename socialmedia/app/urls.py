27b342b9858d540eb797eef48ddd3b9792c02183
karthikvembuluru <meckarthik250@gamil.com>
2024-05-20 15:33:33 +0530

    changes

--- socialmedia/app/urls.py
+++ socialmedia/app/urls.py
@@ -1,7 +1,11 @@
-
 from django.urls import path
 from app.views import *
 
 urlpatterns = [
-    path('',view_profile,name='view_profile')
+     path('',register,name='register'),
+     path('login',login,name='login'),
+     path('logout',logout,name='logout'),
+     path('edit_profile',edit_profile,name='edit_profile'),
+     path('change_password',change_password,name='change_password'),
+     path('view_profile', view_profile, name='view_profile'),
 ]
--- socialmedia/app/views.py
+++ socialmedia/app/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
+# Create your views here.
+def register(request):
+    return render(request,'Register.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def edit_profile(request):
+    return render(request,'edit_profile.html')
+
 def view_profile(request):
     return render(request,'view_profile.html')
+
+def change_password(request):
+    return render(request,'change_password.html')
+
+def logout(request):
+    return render(request,'logout.html')
